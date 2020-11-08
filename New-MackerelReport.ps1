91ee1e328545358f45fc0d2c7a6ace3b34a933ba
nekrassov01 <unclek62@gmail.com>
2020-11-08 21:20:31 +0900

    Update New-MackerelReport.ps1

--- New-MackerelReport.ps1
+++ New-MackerelReport.ps1
@@ -1,6 +1,6 @@
 ﻿# 出力用フォルダを作る
-New-Item -Path "${PSScriptRoot}\report" -ItemType Directory -Force >$null
-New-Item -Path "${PSScriptRoot}\log" -ItemType Directory -Force >$null
+$ReportDir = (New-Item -Path "${PSScriptRoot}\report" -ItemType Directory -Force).FullName
+$LogDir = (New-Item -Path "${PSScriptRoot}\log" -ItemType Directory -Force).FullName
 
 # Start-Processに渡すパラメータをバンドルする
 $Params = @{
@@ -9,16 +9,16 @@ $Params = @{
     FilePath = "C:\Program Files\Python38\Scripts\jupyter-nbconvert.exe"
     
     # 実行ファイルに渡す引数
-    ArgumentList = "--execute ${PSScriptRoot}\mackerel-auto-reporting.ipynb --output ${PSScriptRoot}\report\Mackerel_月次レポート_$((Get-Date).AddMonths(-1).ToString("yyyyMM")).html --to html --no-input --no-prompt --allow-errors -y --template basic --ExecutePreprocessor.kernel_name=python"
+    ArgumentList = "--execute ${PSScriptRoot}\mackerel-auto-reporting.ipynb --output ${ReportDir}\Mackerel_月次レポート_$((Get-Date).AddMonths(-1).ToString("yyyyMM")).html --to html --no-input --no-prompt --allow-errors -y --template basic --ExecutePreprocessor.kernel_name=python"
     
     # 実行ディレクトリ
     WorkingDirectory = ${PSScriptRoot}
 
     # 標準出力のリダイレクト先
-    RedirectStandardOutput = "${PSScriptRoot}\log\stdout_$((Get-Date).ToString("yyyyMMddHHmmss")).log"
+    RedirectStandardOutput = "${LogDir}\stdout_$((Get-Date).ToString("yyyyMMddHHmmss")).log"
 
     # エラー出力のリダイレクト先
-    RedirectStandardError = "${PSScriptRoot}\log\stderr_$((Get-Date).ToString("yyyyMMddHHmmss")).log"
+    RedirectStandardError = "${LogDir}\stderr_$((Get-Date).ToString("yyyyMMddHHmmss")).log"
 }
 
 # コマンドを実行する
